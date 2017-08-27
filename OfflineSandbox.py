e097bb0b41e7fa2f6864c2159dd2ed06182d7968
Jorge Alberto Díaz Orozco <diazorozcoj@gmail.com>
2017-08-27 16:12:04 -0400

    - included autoatic checking if I'm online when needed - included logging -

--- OfflineSandbox.py
+++ OfflineSandbox.py
@@ -1,15 +1,27 @@
 # -*-coding:utf-8-*-
-
+import logging
 from functools import wraps
+from urllib import request
 
-SANDBOX = True
+
+SANDBOX = False
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
 
-def offline_sandbox_value(value):
+def offline_sandbox_value(value, url=None):
     def decorator(func):
         @wraps(func)
         def inner(*args, **kwargs):
-            if SANDBOX:
+            if url:
+                if connect_to(url):
+                    return func(*args, **kwargs)
+                else:
+                    logging.debug('** Sandboxed '+func.__name__+'() with value: '+value)
+                    return value
+
+            elif SANDBOX:
+                logging.debug('** Sandboxed ' + func.__name__ + '() with value: ' + value)
                 return value
             else:
                 return func(*args, **kwargs)
@@ -17,3 +29,11 @@ def offline_sandbox_value(value):
         return inner
 
     return decorator
+
+
+def connect_to(url):
+    try:
+        request.urlopen(url)
+        return True
+    except Exception:
+        return False
--- tests.py
+++ tests.py
@@ -7,6 +7,11 @@ def greet():
     return 'hola'
 
 
+@OfflineSandbox.offline_sandbox_value('mundo', url='https://im-online.herokuapp.com/')
+def greet_with_url():
+    return 'hola'
+
+
 class OfflineSandboxTesting(unittest.TestCase):
     def test_no_sandbox(self):
         OfflineSandbox.SANDBOX = False
@@ -15,6 +20,9 @@ class OfflineSandboxTesting(unittest.TestCase):
     def test_sandbox(self):
         OfflineSandbox.SANDBOX = True
         self.assertEqual(greet(), 'mundo')
+
+    def test_sandbox_url(self):
+        self.assertEqual(greet_with_url(), 'hola')
 
 if __name__ == '__main__':
     unittest.main()
